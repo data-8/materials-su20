878f62c988e8e8d6881223059c33abddd9e4c6aa
Angela Guan <guanangela@berkeley.edu>
2020-07-20 11:09:58 -0700

    publish hw 8

---
+++ materials/su20/hw/hw08/tests/q1_2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'q1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Remember, we would like 2500 bootstrapped estimates. ;
+          >>> # Make sure you are also returning correctly in the function.;
+          >>> len(percentages_in_resamples()) == 2500
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
